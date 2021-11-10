f66b0ca38d4a0dea9b1adb4910edc8161c44fb41
Christopher Pateman <christopher.pateman@version1.com>
2021-11-10 14:46:35 +0000

    wildcard the user details output

--- output.tf
+++ output.tf
@@ -3,17 +3,17 @@ output "product_id" {
 }
 
 output "user_id" {
-  value = azurerm_api_management_user.apim_user[0].id
+  value = azurerm_api_management_user.apim_user[*].id
 }
 output "user_user_id" {
-  value = azurerm_api_management_user.apim_user[0].user_id
+  value = azurerm_api_management_user.apim_user[*].user_id
 }
 output "user_password" {
   value     = random_password.user_password.result
   sensitive = true
 }
 output "user_state" {
-  value = azurerm_api_management_user.apim_user[0].state
+  value = azurerm_api_management_user.apim_user[*].state
 }
 
 output "subscription_id" {
